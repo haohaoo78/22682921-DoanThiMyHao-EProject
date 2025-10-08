54040101a59e97f6317cdcc476938b1ccb8ec086
haohaoo78 <haohaao78@gmail.com>
2025-10-08 21:27:28 +0700

    1

---
+++ api-gateway/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const httpProxy = require("http-proxy");
+
+const proxy = httpProxy.createProxyServer();
+const app = express();
+//Chạy trên Docker thì dùng các tên service
+// Route requests to the auth service
+app.use("/auth", (req, res) => {
+  proxy.web(req, res, { target: "http://auth:3000" });
+});
+
+// Route requests to the product service
+app.use("/products", (req, res) => {
+  proxy.web(req, res, { target: "http://product:3001" });
+});
+
+// Route requests to the order service
+app.use("/orders", (req, res) => {
+  proxy.web(req, res, { target: "http://order:3002" });
+});
+
+// Chạy local thì dùng localhost
+// //Route requests to the auth service
+// app.use("/auth", (req, res) => {
+//   proxy.web(req, res, { target: "http://localhost:3000" });
+// });
+
+// // Route requests to the product service
+// app.use("/products", (req, res) => {
+//   proxy.web(req, res, { target: "http://localhost:3001" });
+// });
+
+// // Route requests to the order service
+// app.use("/orders", (req, res) => {
+//   proxy.web(req, res, { target: "http://localhost:3002" });
+// });
+// Start the server
+const port = process.env.PORT || 3003;
+app.listen(port, () => {
+  console.log(`API Gateway listening on port ${port}`);
+});
